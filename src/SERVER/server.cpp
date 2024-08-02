d6146fba472b6936a89284835042420e832755ac
Kyle <kylehultgren99@yahoo.com>
2024-08-02 13:16:48 -0700

    implemented server check, get ip command, server rdy embed

--- src/SERVER/server.cpp
+++ src/SERVER/server.cpp
@@ -23,6 +23,100 @@ std::vector<std::string> server::get_available_servers()
     return servers;
 }
 
+void server::checkForActiveServer(dpp::cluster& bot)
+{
+    auto potential_servers = get_available_servers();
+    for (std::string server_name : potential_servers)
+    {
+        std::array<char, 32> buffer;
+        std::string line;
+        std::string cmd = "docker ps -q -f \"name=" + server_name + "\"";
+        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+        while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr)
+        {
+            line += buffer.data();
+        }
+        if (!line.empty())
+        {
+            active_container = server_name;
+            bot.log(dpp::loglevel::ll_info, "Found " + server_name + " server!");
+        }
+    }
+}
+
+std::string server::getIP()
+{
+    std::string ip = "???.???.???.???";
+    std::array<char, 50> buffer;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("hostname -I", "r"), pclose);
+    if (pipe)
+    {
+        std::string res;
+        while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr)
+            res += buffer.data();
+        size_t ind = res.find(' ');
+        if (ind != std::string::npos)
+            ip = res.substr(0, ind);
+        else
+        {
+            res.pop_back();
+            ip = res;
+        }
+    }
+    return ip;
+}
+
+dpp::embed server::createServerEmbed()
+{
+    dpp::embed server_embed = dpp::embed()
+        .set_color(dpp::colors::pink_rose)
+        .set_title("Your server is ready!")
+        .set_thumbnail(MIKU_THUMBNAIL)
+        .set_footer(
+            dpp::embed_footer()
+            .set_text("Jesus loves you!")
+        )
+        .set_timestamp(time(0));
+
+    YAML::Node config = YAML::LoadFile(server_dir + "/" + active_container + "/docker-compose.yml");
+    YAML::Node service = config["services"].begin()->second;
+
+    // minecraft server
+    if (active_container == "minecraft")
+    {
+        YAML::Node env = service["environment"];
+        if (env)
+        {
+            server_embed.add_field("Game", "Minecraft");
+            if (env["CF_SLUG"])
+                server_embed.add_field("Mod Pack", env["CF_SLUG"].as<std::string>());
+            if (env["VERSION"])
+                server_embed.add_field("Game Version", env["VERSION"].as<std::string>());
+        }
+    }
+
+    return server_embed;
+}
+
+std::string server::getPort()
+{
+    std::string port = "????";
+    if (!active_container.empty())
+    {
+        std::string path = server_dir + "/" + active_container + "/docker-compose.yml";
+        YAML::Node config = YAML::LoadFile(path);
+        YAML::Node service = config["services"].begin()->second;
+        if (service["ports"])
+        {
+            std::string line = service["ports"][0].as<std::string>();
+            size_t ind = line.find(':');
+            if (ind != std::string::npos)
+                port = line.substr(ind+1);
+        }
+    }
+    return port;
+}
+
 void server::start(const dpp::slashcommand_t& event)
 {   
     if (active_container.empty())
@@ -41,17 +135,26 @@ void server::start(const dpp::slashcommand_t& event)
                     return;
                 }
 
-                event.reply("Starting " + server_name + " server. Please wait...");
-                std::string cmd = "docker compose -f " + path + "/docker-compose.yml up -d --wait";
-                int res = system(cmd.c_str());
-                if (res == 0)
-                {
-                    // TODO print more useful info like ip and stuff                    
-                    event.edit_original_response(dpp::message(server_name + " server started successfuly!"));
-                    active_container = server_name;
-                }
-                else
-                    event.edit_original_response(dpp::message("Could not start server"));
+                event.reply("Starting " + server_name + " server. Please wait...",
+                [event, server_name, path](const dpp::confirmation_callback_t& callback){
+                    if (!callback.is_error())
+                    {
+                        std::string cmd = "docker compose -f " + path + "/docker-compose.yml up -d --wait";
+                        int res = system(cmd.c_str());
+                        if (res == 0)
+                        {
+                            // TODO print more useful info like ip and stuff                
+                            active_container = server_name;    
+                            event.edit_original_response(dpp::message(createServerEmbed()));
+                        }
+                        else
+                        {
+                            cmd = "docker compose -f " + path + "/docker-compose.yml stop";
+                            event.edit_original_response(dpp::message("Could not start server"));
+                            system(cmd.c_str());
+                        }
+                    }
+                });
             }
             else
                 event.reply("Please provide name for server you want to start");
@@ -70,37 +173,76 @@ void server::terminate(const dpp::slashcommand_t& event)
     if (!active_container.empty())
     {
         std::string path = server_dir + "/" + active_container + "/docker-compose.yml";
-        std::string cmd = "docker compose -f " + path + " stop";
-        event.reply("Shutting down " + active_container + " server. Please wait...");
-        int res = system(cmd.c_str());
-        if (res == 0)
-        {
-            bool restart;
-            try { restart = std::get<bool>(event.get_parameter("restart")); }
-            catch(const std::exception& e){ restart = false; }
-             
-            if (restart)
+        event.reply("Shutting down " + active_container + " server. Please wait...",
+        [event, path](const dpp::confirmation_callback_t& callback){
+            std::string cmd = "docker compose -f " + path + " stop";
+            int res = system(cmd.c_str());
+            if (res == 0)
             {
-                event.edit_original_response(dpp::message("Restarting " + active_container + " server. Please wait..."));
-                cmd = "docker compose -f " + path + " up -d --wait";
-                res = system(cmd.c_str());
-                if (res == 0)
+                bool restart;
+                try { restart = std::get<bool>(event.get_parameter("restart")); }
+                catch(const std::exception& e){ restart = false; }
+
+                if (restart)
                 {
-                    event.edit_original_response(dpp::message(active_container + " server started successfuly!"));
+                    event.edit_original_response(dpp::message("Restarting " + active_container + " server. Please wait..."));
+                    cmd = "docker compose -f " + path + " up -d --wait";
+                    res = system(cmd.c_str());
+                    if (res == 0)
+                    {
+                        event.edit_original_response(dpp::message(createServerEmbed()));
+                    }
+                    else
+                    {
+                        cmd = "docker compose -f " + path + "/docker-compose.yml stop";
+                        event.edit_original_response(dpp::message("There was an issue restarting the server"));
+                        system(cmd.c_str());
+                    }
                 }
                 else
                 {
-                    event.edit_original_response(dpp::message("There was an issue restarting the server"));
+                    event.edit_original_response(dpp::message(active_container + " server stopped successfully"));
                     active_container = "";
                 }
             }
-            else
-            {
-                event.edit_original_response(dpp::message(active_container + " server stopped successfully"));
-                active_container = "";
-            }
-        }
+        });
     }
     else
         event.reply("No server currently running. Start one with /start <name>");
+}
+
+void server::ip(dpp::cluster& bot, const dpp::slashcommand_t& event)
+{
+    if (!active_container.empty())
+    {
+        dpp::user user = event.command.get_issuing_user();
+        auto guild = event.command.get_guild();
+        dpp::guild_member user_guild = guild.members[user.id];
+        auto roles = user_guild.get_roles();
+        bool chill = false;
+        for (auto role: roles)
+        {
+            if (role.str() == "718598319537913859")
+            {
+                chill = true;
+                break;
+            }
+        }
+
+        if (chill)
+        {
+            bot.direct_message_create(user.id, dpp::message("Here's the ip for the " + active_container + " server\n" + getIP() + ":" + getPort()),
+            [event](const dpp::confirmation_callback_t& callback)
+            {
+                if (callback.is_error())
+                    event.reply("Smth went wrong. Could not slide into your DM");
+                else
+                    event.reply("Check your DM pookie");
+            });
+        }
+        else
+            event.reply("You're not allowed to see my ip");
+    }
+    else
+        event.reply("No active server");
 }
--- src/SERVER/server.h
+++ src/SERVER/server.h
@@ -7,19 +7,25 @@
 #include <vector>
 
 #define SERVER_LIST_FILE "servers.txt"
+#define MIKU_THUMBNAIL "https://assets.audiomack.com/ghxstnx/c31bf1a8c0b277651cb1e02630598e57afb31857ebec32992842e66de6bac31e.jpeg?width=300"
 
 /*Will handle all functionality related to starting, stopping, and configuring game servers*/
 class server
 {
     public:
         static std::vector<std::string> get_available_servers();
+        static void checkForActiveServer(dpp::cluster& bot);
         static void setDir(std::string dir) { server_dir = dir; }
         static bool dirExists() { return !server_dir.empty(); }
         static void start(const dpp::slashcommand_t& event);
         static void terminate(const dpp::slashcommand_t& event);
+        static void ip(dpp::cluster& bot, const dpp::slashcommand_t& event);
     private:
         static std::string server_dir;
         static std::string active_container;
+        static std::string getIP();
+        static std::string getPort();
+        static dpp::embed createServerEmbed();
 };
 
 #endif
